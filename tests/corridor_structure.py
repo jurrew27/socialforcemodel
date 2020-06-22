a5d0491d3349c45dabd12cb02581e53234f6d619
Jurre Wolsink <jurre.wolsink@gmail.com>
2020-06-22 14:09:03 +0200

    Divide (iteration, rate) pairs evenly per thread

--- tests/corridor_structure.py
+++ tests/corridor_structure.py
@@ -1,58 +1,65 @@
 import socialforcemodel as sfm
 import matplotlib.pyplot as plt
 import os
-from tqdm import tqdm
+from tqdm import trange
 from multiprocessing import Pool
+from itertools import product
+from functools import partial
 
-def main(args, iteration):
-    out_dir = f'{args.outfile}_{iteration}'
 
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+def simulate(args, iteration, rate):
+    loader = sfm.ParameterLoader(args.file)
+    world = loader.world
+    world.update()
 
-    for rate in args.rates:
-        out_dir_img = f'{out_dir}/rate_{rate}_img'
+    log_interval = 0.1
+    log_every = round(log_interval / world.step_size)
 
-        if not os.path.exists(out_dir_img):
-            os.makedirs(out_dir_img)
+    for group in world.groups:
+        group.set_ornstein_uhlenbeck_process(0, 0.15, 0.01)
+        group.set_spawn_rate(rate)
 
-        loader = sfm.ParameterLoader(args.file)
-        world = loader.world
+    rows = []
+
+    for step in trange(args.steps, desc=f'Iteration {iteration}, rate {rate}'):
+        if not world.step():
+            break
+
         world.update()
+        if step % log_every == 0:
+            figure = world.plot()
+            figure.savefig(f'{args.outfile}_{iteration}/rate_{rate}_img/%03d.png' % ((step + 1) // log_every),
+                           bbox_inches='tight',
+                           pad_inches=0.1)
+            figure.clear()
+            plt.close(figure)
 
-        log_interval = 0.1
-        log_every = round(log_interval / world.step_size)
+            pedestrians = world.quadtree.get_pedestrians()
+            for p in pedestrians:
+                rows.append([world.time, p.id, p.group.id, p.position[0], p.position[1], p.speed])
 
-        for group in world.groups:
-            group.set_ornstein_uhlenbeck_process(0, 0.15, 0.01)
-            group.set_spawn_rate(rate)
+    with open(f'{args.outfile}_{iteration}/rate_{rate}.csv', 'w') as outfile:
+        import csv
+        writer = csv.writer(outfile)
+        writer.writerow(['time', 'pedestrian_id', 'group_id', 'x', 'y', 'velocity'])
+        for row in rows:
+            writer.writerow(row)
 
-        rows = []
 
-        for step in tqdm(range(args.steps), desc=f'Iteration {iteration}, rate {rate}'):
-            if not world.step():
-                break
+def main(args):
+    for iteration in range(args.iterations):
+        out_dir = f'{args.outfile}_{iteration}'
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
 
-            world.update()
-            if step % log_every == 0:
-                figure = world.plot()
-                figure.savefig(f'{out_dir_img}/%03d.png' % ((step + 1) // log_every),
-                               bbox_inches = 'tight',
-                               pad_inches = 0.1)
-                figure.clear()
-                plt.close(figure)
+        for rate in args.rates:
+            out_dir_img = f'{out_dir}/rate_{rate}_img'
+            if not os.path.exists(out_dir_img):
+                os.makedirs(out_dir_img)
 
-            if step % log_every == 0:
-                pedestrians = world.quadtree.get_pedestrians()
-                for p in pedestrians:
-                    rows.append([world.time, p.id, p.group.id, p.position[0], p.position[1], p.speed])
-
-        with open(f'{out_dir}/rate_{rate}.csv', 'w') as outfile:
-            import csv
-            writer = csv.writer(outfile)
-            writer.writerow(['time', 'pedestrian_id', 'group_id', 'x', 'y', 'velocity'])
-            for row in rows:
-                writer.writerow(row)
+    iterable = product(range(args.iterations), args.rates)
+    with Pool() as p:
+        p.starmap(partial(simulate, args), iterable)
 
 
 if __name__ == '__main__':
@@ -66,5 +73,4 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--iterations', help='Number of iterations', type=int, default=1)
     args = parser.parse_args(sys.argv[1:])
 
-    with Pool() as p:
-        p.starmap(main, zip([args]*args.iterations, range(args.iterations)))
+    main(args)
