0ac64f337abc795a34631fe7408c9a899d270a69
hensjiepensjie <hanspijpers58@hotmail.com>
2020-06-23 12:52:59 +0200

    Cluster analysis DBSCAN

--- tests/cluster_analysis.py
+++ tests/cluster_analysis.py
@@ -38,17 +38,6 @@ def analyze_data(args):
 
 def main(args):
     analyze_data(args)
-    from sklearn.datasets import make_blobs
-    # create blobs
-    # data = make_blobs(n_samples=200, n_features=2, centers=4, cluster_std=1.6,
-    #                  random_state=50)  # create np array for data points
-    # points = data[0]  # create scatter plot
-    # print(data)
-    # print(points)
-    # plt.scatter(data[0][:, 0], data[0][:, 1], c=data[1], cmap='viridis')
-    # plt.xlim(-15, 15)
-    # plt.ylim(-15, 15)
-    # plt.show()
 
 
 if __name__ == '__main__':
