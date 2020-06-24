b6fa5b733c8240ef73119aedec2c58219c3351c7
hensjiepensjie <hanspijpers58@hotmail.com>
2020-06-24 16:07:44 +0200

    Added calculation of proper velocities in vector form

--- tests/velocity_analysis.py
+++ tests/velocity_analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
@@ -7,6 +8,7 @@ def analyze_data(args):
     mean_velocities = []
     for f in args.file:
         df = pd.read_csv(f, sep=",")
+        df = calculate_real_velocities(args, df)
         # Eliminate trailing zeroes from floats in time
         df.time = df.time.round(2)
 
@@ -48,6 +50,30 @@ def analyze_data(args):
     #    for row in rows:
     #        writer.writerow(row)
 
+def calculate_real_velocities(args, data):
+
+    for id_ped in range(data.pedestrian_id.min(), data.pedestrian_id.max()):
+
+        temp_df = data.loc[data["pedestrian_id"] == id_ped]
+
+        vec_x = temp_df['x'].values[1:]
+        vec_old_x = temp_df['x'].values[:-1]
+        vec_y = temp_df['y'].values[1:]
+        vec_old_y = temp_df['y'].values[:-1]
+
+        delta_x = abs(vec_x - vec_old_x)
+        delta_y = abs(vec_y - vec_old_y)
+
+        displacement = list(map(math.sqrt, delta_x**2 + delta_y**2))
+        time_elapsed = temp_df['time'].values[1:] - temp_df['time'].values[:-1]
+        proper_velocity = displacement / time_elapsed
+
+        # Insert first value into list as workaround, as i cant get pandas to give me [1:] of the velocity list of the pedastrian
+        proper_velocity = np.insert(proper_velocity, 0, temp_df['velocity'].values[0])
+
+        data["velocity"].loc[data["pedestrian_id"] == id_ped] = proper_velocity
+
+    return data
 
 def main(args):
     #out_dir = f'{args.outfile}_{args.iterations}'
