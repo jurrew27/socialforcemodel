98af8276bf17aeb50e25e7a5c76b6bb054dd96dc
hensjiepensjie <hanspijpers58@hotmail.com>
2020-06-24 21:43:59 +0200

    Changed all images to videos from seperate

--- tests/create_videos.py
+++ tests/create_videos.py
@@ -1,6 +1,10 @@
 import subprocess
 import numpy as np
+import os
 
-for i in range(5):
-    for rate in np.arange(0.1, 1.01, 0.1):
-        subprocess.run(['ffmpeg', '-i',  f'measurements_{i}/rate_{rate}_img/%03d.png', '-pix_fmt', 'yuv420p', f'measurements_{i}/rate_{rate}.mp4'])
+print(os.getcwd())
+
+for i in range(10):
+    for rate in [0.3, 0.7]:
+
+        subprocess.run(['ffmpeg', '-i', f'seperate_{i}/rate_{rate}_img/%03d.png', '-pix_fmt', 'yuv420p', f'seperate_{i}/rate_{rate}.mp4'])
